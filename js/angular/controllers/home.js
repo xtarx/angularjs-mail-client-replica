dd8f640f812d677cf6705c7ed8500005e9f67741
xtarx <xtarx0@gmail.com>
2015-07-22 22:45:59 +0200

    .render emails from server .show email message modal in a content when clicked

--- js/angular/controllers/home.js
+++ js/angular/controllers/home.js
@@ -2,11 +2,42 @@
 
 var app = angular.module('myApp');
 app.controller('HomeController', ['$scope', function ($scope) {
+    $scope.selectedMail;
+    $scope.setSelectedMail = function (mail) {
+        $scope.selectedMail = mail;
+    };
 
+    $scope.isSelected = function (mail) {
+        if ($scope.selectedMail) {
+            return $scope.selectedMail === mail;
+        }
+    };
 	}]);
 app.controller('SettingsController', ['$scope', function ($scope) {
-
+    $scope.settings = {
+        name: "Ari",
+        email: "me@example.com"
+    }
     $scope.updateSettings = function () {
         console.log("form has been submitted")
     }
 	}]);
+
+app.controller('MailListingController', ['$scope', '$http', function ($scope, $http) {
+    $scope.email = [];
+    $http({
+            method: 'GET',
+            url: '/api/mail'
+        })
+        .success(function (data, status, headers) {
+            $scope.email = data.all;
+        }).error(function (data, status, headers) {
+
+        })
+}]);
+app.controller('ContentController', ['$scope', function ($scope) {
+    $scope.showingReply=false;
+    $scope.showReply=function(){
+        $scope.showingReply=true;
+    };
+}]);
